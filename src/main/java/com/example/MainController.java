89cf0dd0f219b0e6e640998b32dde2379a305287
Jungels_Aiden <junga526@hsd.k12.or.us>
2025-05-22 09:44:02 -0700

    connected all controls to java controller file

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -1,8 +1,92 @@
 package com.example;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+//import java.lang.classfile.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+
+public class MainController implements Initializable{
+
+    // Generation Controls
+    @FXML
+    private Button genreButton;
+
+    @FXML
+    private Label genreLabel;
+
+    @FXML
+    private Button keyButton;
+
+    @FXML
+    private Label keyLabel;
+
+    // Time Selection lists
+    @FXML
+    private ListView<Integer> rList;
+
+    @FXML
+    private ListView<Integer> sList;
+
+    // Research Timer Control
+    @FXML
+    private Button rStartButton;
+
+    @FXML
+    private Button rPauseButton;
+
+    @FXML
+    private Button rStopButton;
+
+    @FXML
+    private Label rTimer;
+
+    // Song Timer Control
+    @FXML
+    private Button sStartButton;
+
+    @FXML
+    private Button sPauseButton;
+
+    @FXML
+    private Button sStopButton;
     
+    @FXML
+    private Label sTimer;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+        rList.getItems().add(1);
+        rList.getItems().add(2);
+        rList.getItems().add(3);
+        rList.getItems().add(4);
+        rList.getItems().add(5);
+
+        sList.getItems().add(1);
+        sList.getItems().add(2);
+        sList.getItems().add(3);
+        sList.getItems().add(4);
+        sList.getItems().add(5);
+    }
+
+    @FXML
+    void generateGenre() {
+        genreLabel.setText("genre worked");
+        System.out.print("genre worked");
+    }
+
+    @FXML
+    void generateKey() {
+        keyLabel.setText("key worked");
+        System.out.print("key worked");
+    }
+
 }
